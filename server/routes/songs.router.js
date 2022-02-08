58d0260d62f0b4db4fa9247447a7902df13590f9
Tony LaForgia <alforgia428@gmail.com>
2022-02-08 14:53:14 -0600

    Postman working

--- server/routes/songs.router.js
+++ server/routes/songs.router.js
@@ -1,34 +1,66 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const pg = require("pg");
 
-let songs = [
-    {
-        rank: 355, 
-        artist: 'Ke$ha', 
-        track: 'Tik-Toc', 
-        published: '1/1/2009'
-    },
-    {
-        rank: 356, 
-        artist: 'Gene Autry', 
-        track: 'Rudolph, the Red-Nosed Reindeer', 
-        published: '1/1/1949'
-    },
-    {
-        rank: 357, 
-        artist: 'Oasis', 
-        track: 'Wonderwall', 
-        published: '1/1/1996'
-    }
-];
+const Pool = pg.Pool;
 
-router.get('/', (req, res) => {
-    res.send(songs);
+const pool = new Pool({
+  database: "music_library", // the name of the database
+  host: "localhost", // where our database is
+  port: 5432, // the port for your db; 5432 is your default fro postgres
+  max: 10, // how many connections (queries) at one time
+  idleTimeoutMillis: 30000, // 30 seconds to try to connect, then cancel query
 });
 
-router.post('/', (req, res) => {
-    songs.push(req.body);
-    res.sendStatus(200);
+// is not required, but is useful for debugging
+pool.on("connect", () => {
+  console.log("PostgreSQL is connected!");
+});
+
+// the pool will emit an error on behalf of any idle clients
+pool.on("error", (error) => {
+  console.log("Error with postgres pool", error);
+});
+
+let songs = [
+  {
+    rank: 355,
+    artist: "Ke$ha",
+    track: "Tik-Toc",
+    published: "1/1/2009",
+  },
+  {
+    rank: 356,
+    artist: "Gene Autry",
+    track: "Rudolph, the Red-Nosed Reindeer",
+    published: "1/1/1949",
+  },
+  {
+    rank: 357,
+    artist: "Oasis",
+    track: "Wonderwall",
+    published: "1/1/1996",
+  },
+];
+
+router.get("/", (req, res) => {
+  //   res.send(songs);
+  // check SQL query text in Postico first!
+  let queryText = 'SELECT * FROM "songs";';
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("Error making query", queryText, err);
+      res.sendStatus(500);
+    });
+});
+
+router.post("/", (req, res) => {
+  songs.push(req.body);
+  res.sendStatus(200);
 });
 
 module.exports = router;
